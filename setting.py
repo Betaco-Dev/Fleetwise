5f07e2a624266d756d8fd5a29738d05be342812d
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-10 17:49:22 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -11,6 +11,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
   'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+  'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  # Token Authentication
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Require authentication by default
+    ],
 }
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Fleet Management API',
