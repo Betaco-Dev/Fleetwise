2a3532cf8d6b480fc7afba59bf32a8afc6c91fb4
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-10 11:52:37 +0300

    Create auditlog.py

---
+++ models/auditlog.py
@@ -0,0 +1,7 @@
+class AuditLog(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    action = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.action} at {self.timestamp}"
