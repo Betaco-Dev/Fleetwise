372b3d15de7fb1367f39bc3ef6d090183c503522
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-10 11:50:15 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,17 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from encrypted_model_fields.fields import EncryptedCharField
 
-class User(models.Model):
-    ROLE_CHOICES = [
-        ('Driver', 'Driver'),
-        ('Rider', 'Rider'),
-    ]
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    phone = models.CharField(max_length=20)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Rider')
-    license_number = models.CharField(max_length=50)
+class User(AbstractUser):
+    phone = EncryptedCharField(max_length=15, blank=True, null=True)
+    # Rest of the fields...
+    role = models.CharField(
+        max_length=10,
+        choices=[
+            ('Driver', 'Driver'),
+            ('Rider', 'Rider'),
+        ],
+        default='Rider'
+    )
+    license_number = models.CharField(max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.username
