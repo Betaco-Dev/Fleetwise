f8e51c6a4f115cc8451247a3800e0283e82c1b9d
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-10 12:57:08 +0300

    Update url.py

--- url.py
+++ url.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import RateLimitedLoginView
+from . import views
 
 urlpatterns = [
     path('login/', RateLimitedLoginView.as_view(), name='login'),
-    # Other URLs...
+    path('predict-maintenance/', views.predict_maintenance_view, name='predict_maintenance'),
+    path('detect-anomalies/', views.detect_anomalies_view, name='detect_anomalies'),
 ]
