16aba90e8687ee3a7efe2951122eaae35e15496b
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-12 13:22:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class YourAppNameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'your_app_name'
+    name = 'Fleetwise'
 
     def ready(self):
-        import your_app_name.signals  # Replace `your_app_name` with your actual app name
+        import Fleetwise.signals  # Replace `your_app_name` with your actual app name
