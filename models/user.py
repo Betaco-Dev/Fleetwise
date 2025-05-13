5c23254ac76a58eef8f4a7d071029dd936550822
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-13 12:59:34 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -5,28 +5,35 @@ from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 
+
+class RoleChoices(models.TextChoices):
+    DRIVER = 'Driver', _('Driver')
+    RIDER = 'Rider', _('Rider')
+
+
 class User(AbstractUser):
     phone_validator = RegexValidator(
         regex=r'^\+?1?\d{9,15}$',
-        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+        message=_("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     )
     phone = EncryptedCharField(max_length=15, blank=True, null=True, validators=[phone_validator])
     
     role = models.CharField(
         max_length=10,
-        choices=[
-            ('Driver', _('Driver')),
-            ('Rider', _('Rider')),
-        ],
-        default='Driver'
+        choices=RoleChoices.choices,
+        default=RoleChoices.DRIVER  # Set default role to Driver
     )
     license_number = models.CharField(max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def clean(self):
-        if self.role == 'Driver', 'Rider'and not self.license_number:
-            raise ValidationError("License number is required for drivers.")
+        """
+        Custom validation for the User model.
+        Ensures that both drivers and riders must provide a license number.
+        """
+        if not self.license_number:
+            raise ValidationError(_("License number is required for registration."))
 
     def __str__(self):
         return self.username
@@ -36,4 +43,5 @@ class User(AbstractUser):
             models.Index(fields=['username']),
             models.Index(fields=['email']),
             models.Index(fields=['role']),
+            models.Index(fields=['phone']),  # Added index on phone
         ]
