8e37c778371623826ad9b9a17e959d34ed73e969
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 11:05:49 +0300

    Update and rename trackinglogs.py to tracking_log.py

--- URLs/tracking_log.py
+++ URLs/tracking_log.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TrackingLogViewSet
 
 router = DefaultRouter()
-router.register(r'tracking-logs', TrackingLogViewSet, basename='trackinglog')
+router.register(r'tracking-log', TrackingLogViewSet, basename='tracking_log')
 
 urlpatterns = [
     path('', include(router.urls)),
