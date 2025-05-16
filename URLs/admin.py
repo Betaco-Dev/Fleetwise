ffadd00b03cfbc6f8e383d6fa1ec2911b13061c3
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 09:52:41 +0300

    Update admin.py

--- URLs/admin.py
+++ URLs/admin.py
@@ -1,1 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AdminViewSet  # change this to match your ViewSet
 
+router = DefaultRouter()
+router.register(r'admin', AdminViewSet, basename='admin')  # adjust 'users' and 'user' as needed
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
