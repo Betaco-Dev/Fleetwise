be6d9a91a680b69017327b2e134f4388f9c9bf82
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 15:33:32 +0300

    Create currency.py

---
+++ Fleetwise/main/views/currency.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Currency
+from .serializers import CurrencySerializer
+
+class CurrencyViewSet(ModelViewSet):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
