158b21009a756125dcd568152fb7da6c81cdb255
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 08:51:13 +0300

    Update analyticsreporting.py

--- views/analyticsreporting.py
+++ views/analyticsreporting.py
@@ -1,1 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Analytics
+from .serializers import AnalyticsSerializer
 
+class AnalyticsViewSet(ModelViewSet):
+    queryset = Analytics.objects.all()
+    serializer_class = AnalyticsSerializer
