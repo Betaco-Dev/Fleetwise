f23fc20fd99e295eff01582540e847cd10881d0f
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 11:05:06 +0300

    Update and rename routeplan.py to route_plan.py

--- URLs/route_plan.py
+++ URLs/route_plan.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import RoutePlanViewSet
 
 router = DefaultRouter()
-router.register(r'route-plan', RoutePlanViewSet, basename='routeplan')
+router.register(r'route-plan', RoutePlanViewSet, basename='route_plan')
 
 urlpatterns = [
     path('', include(router.urls)),
