380dc5ef48babd2dbc1b6a126535646cd6d6d04a
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 11:09:29 +0300

    Update vehicles.py

--- URLs/vehicles.py
+++ URLs/vehicles.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import VehicleViewSet
 
 router = DefaultRouter()
-router.register(r'vehicles', VehicleViewSet, basename='vehicle')
+router.register(r'vehicle', VehicleViewSet, basename='vehicle')
 
 urlpatterns = [
     path('', include(router.urls)),
