40f5b6fb945b344959e6a9e5b72ff7c2509c0e18
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 08:59:26 +0300

    Update routeoptimization.py

--- views/routeoptimization.py
+++ views/routeoptimization.py
@@ -1,1 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import RouteOptimization
+from .serializers import RouteOptimizationSerializer
 
+class RouteOptimizationViewSet(ModelViewSet):
+    queryset = RouteOptimization.objects.all()
+    serializer_class = RouteOptimizationSerializer
