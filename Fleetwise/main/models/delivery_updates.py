3aeff067355aa4c60e8047459875ff7b50caa62e
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 15:11:17 +0300

    Create delivery_updates.py

---
+++ Fleetwise/main/models/delivery_updates.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .user import User
+from .vehicle import Vehicle
+
+class DeliveryUpdate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, null=True, blank=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=100)
+    update_timestamp = models.DateTimeField()
+
+  def __str__(self):
+    return f"Delivery Update {self.id} - {self.user.name}"
