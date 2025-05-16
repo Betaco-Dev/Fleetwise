e845f903060390e6296609d62d2d90ac8f2f1b22
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 08:57:58 +0300

    Update otherexpenses.py

--- views/otherexpenses.py
+++ views/otherexpenses.py
@@ -1,1 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import OtherExpenses
+from .serializers import OtherExpensesSerializer
 
+class OtherExpensesViewSet(ModelViewSet):
+    queryset = OtherExpenses.objects.all()
+    serializer_class = OtherExpensesSerializer
