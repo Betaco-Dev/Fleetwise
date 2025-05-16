63aa7f9841893578ee79e5c19d66df48112b8cbb
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 10:10:26 +0300

    Create weathercondition.py

---
+++ URLs/weathercondition.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import WeatherConditionViewSet
+
+router = DefaultRouter()
+router.register(r'weather-conditions', WeatherConditionViewSet, basename='weathercondition')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
