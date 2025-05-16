eb870a07313e7e102809ab4ab6218dccc7611106
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 11:12:44 +0300

    Update weathercondition.py

--- URLs/weathercondition.py
+++ URLs/weathercondition.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import WeatherViewSet
 
 router = DefaultRouter()
-router.register(r'weather-conditions', WeatherConditionViewSet, basename='weathercondition')
+router.register(r'weather', WeatherViewSet, basename='weather')
 
 urlpatterns = [
     path('', include(router.urls)),
