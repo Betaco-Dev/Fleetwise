3d0ea1ad813c3e12fb5b422f3e36915fbdbf5bc4
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 09:54:30 +0300

    Update admin.py

--- URLs/admin.py
+++ URLs/admin.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import AdminViewSet  # change this to match your ViewSet
 
 router = DefaultRouter()
-router.register(r'admin', AdminViewSet, basename='admin')  # adjust 'users' and 'user' as needed
+router.register(r'admins', AdminViewSet, basename='admin')  # adjust 'users' and 'user' as needed
 
 urlpatterns = [
     path('', include(router.urls)),
