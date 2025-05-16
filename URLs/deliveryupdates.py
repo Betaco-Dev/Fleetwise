65f54b405e3c3d3287161f9de842780681aa44ba
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 10:05:22 +0300

    Create deliveryupdates.py

---
+++ URLs/deliveryupdates.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DeliveryUpdatesViewSet
+
+router = DefaultRouter()
+router.register(r'delivery-updates', DeliveryUpdatesViewSet, basename='deliveryupdates')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
