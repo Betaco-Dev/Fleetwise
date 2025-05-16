d224c9b6ebb94d41288b7077cd2405061bd6060d
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 11:08:17 +0300

    Update users.py

--- URLs/users.py
+++ URLs/users.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet  # change this to match your ViewSet
+from .views import UsersViewSet  # change this to match your ViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')  # adjust 'users' and 'user' as needed
+router.register(r'users', UserViewSet, basename='users')  # adjust 'users' and 'user' as needed
 
 urlpatterns = [
     path('', include(router.urls)),
