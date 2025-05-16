511b4998c1f88771592cdf03c1bca9502a5a52f6
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 11:11:57 +0300

    Update weathercondition.py

--- URLs/weathercondition.py
+++ URLs/weathercondition.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import WeatherConditionViewSet
+from .views import WeatherViewSet
 
 router = DefaultRouter()
 router.register(r'weather-conditions', WeatherConditionViewSet, basename='weathercondition')
