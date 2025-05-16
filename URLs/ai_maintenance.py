8ae2e5941f8588119c76cabc70750b3e20689b7d
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 11:02:16 +0300

    Update ai_maintenance.py

--- URLs/ai_maintenance.py
+++ URLs/ai_maintenance.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AiMaintenanceViewSet
+from .views import MaintenancePredictionViewSet
 
 router = DefaultRouter()
-router.register(r'ai-maintenance', PredictionViewSet, basename='ai_maintenance')
+router.register(r'maintenance-prediction', PredictionViewSet, basename='maintenance_prediction')
 
 urlpatterns = [
     path('', include(router.urls)),
