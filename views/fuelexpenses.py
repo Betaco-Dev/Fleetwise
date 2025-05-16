95f57ac010a388e07f0e45068a8fa091a6477535
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 11:43:30 +0300

    Update and rename fuelexpenses.py to fuel_expense.py

--- views/fuelexpenses.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import FuelExpenses
-from .serializers import FuelExpensesSerializer
-
-class FuelExpensesViewSet(ModelViewSet):
-    queryset = FuelExpenses.objects.all()
-    serializer_class = FuelExpensesSerializer
