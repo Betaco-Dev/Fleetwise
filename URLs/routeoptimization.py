da3dc4cbf9c417fb3a40715a4d07a83e4cc2a07a
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 10:59:01 +0300

    Update routeoptimization.py

--- URLs/routeoptimization.py
+++ URLs/routeoptimization.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import RouteOptimizationViewSet
 
 router = DefaultRouter()
-router.register(r'route-optimization', RouteOptimizationViewSet, basename='routeoptimization')
+router.register(r'route-optimization', RouteOptimizationViewSet, basename='route_optimization')
 
 urlpatterns = [
     path('', include(router.urls)),
