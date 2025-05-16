27cfa42937ffb59d32f5a07c1f5d4e1407c2892f
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 08:56:36 +0300

    Update maintenanceschedule.py

--- views/maintenanceschedule.py
+++ views/maintenanceschedule.py
@@ -1,1 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import MaintenanceSchedule
+from .serializers import MaintenanceScheduleSerializer
 
+class MaintenanceScheduleViewSet(ModelViewSet):
+    queryset = MaintenanceSchedule.objects.all()
+    serializer_class = MaintenanceScheduleSerializer
