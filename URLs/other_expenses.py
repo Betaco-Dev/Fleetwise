7ff773bcdaa7e36d59f95662cfe73082f31dcf82
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 10:59:36 +0300

    Update other_expenses.py

--- URLs/other_expenses.py
+++ URLs/other_expenses.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OtherExpensesViewSet
+from .views import OtherExpenseViewSet
 
 router = DefaultRouter()
-router.register(r'other-expense', OtherExpensesViewSet, basename='otherexpense')
+router.register(r'other-expense', OtherExpensesViewSet, basename='other_expense')
 
 urlpatterns = [
     path('', include(router.urls)),
