604faec3b4097e426c98badf6db493f1eebd322c
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 08:52:35 +0300

    Update deliveryupdates.py

--- views/deliveryupdates.py
+++ views/deliveryupdates.py
@@ -1,1 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import DeliveryUpdates
+from .serializers import DeliveryUpdatesSerializer
 
+class DeliveryUpdatesViewSet(ModelViewSet):
+    queryset = DeliveryUpdates.objects.all()
+    serializer_class = DeliveryUpdatesSerializer
