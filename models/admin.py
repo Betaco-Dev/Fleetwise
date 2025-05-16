2295d2ac0751f25502e48b747408a0ab4e01704a
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 15:19:37 +0300

    Delete models directory

--- models/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'role', 'phone', 'license_number', 'created_at', 'updated_at')
-    list_filter = ('role', 'is_active', 'is_staff')
-    search_fields = ('username', 'email', 'phone')
--- models/currency.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Currency(models.Model):
-    currency_code = models.CharField(max_length=3, primary_key=True)
-    currency_name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.currency_name
