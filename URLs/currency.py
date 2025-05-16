f2763cf58e535ed2ce9fa432b5a60683d4124906
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 10:40:48 +0300

    Update currency.py

--- URLs/currency.py
+++ URLs/currency.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import CurrencyViewSet
 
 router = DefaultRouter()
-router.register(r'currencies', CurrencyViewSet, basename='currency')
+router.register(r'currency', CurrencyViewSet, basename='currency')
 
 urlpatterns = [
     path('', include(router.urls)),
