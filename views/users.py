a4197356d7140ff66b3859f7a99ef6dd10d2adc2
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 09:01:20 +0300

    Update users.py

--- views/users.py
+++ views/users.py
@@ -1,1 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Users
+from .serializers import UsersSerializer
 
+class UsersViewSet(ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
