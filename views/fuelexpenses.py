59aeaf9dc0947c3d7ba396d5dc62b0cf052aa260
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 08:53:50 +0300

    Update fuelexpenses.py

--- views/fuelexpenses.py
+++ views/fuelexpenses.py
@@ -1,1 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import FuelExpenses
+from .serializers import FuelExpensesSerializer
 
+class FuelExpensesViewSet(ModelViewSet):
+    queryset = FuelExpenses.objects.all()
+    serializer_class = FuelExpensesSerializer
