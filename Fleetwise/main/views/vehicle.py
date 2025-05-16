9433ac513de5683afcb78440f2576b2ba9c0b125
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 15:41:15 +0300

    Create vehicle.py

---
+++ Fleetwise/main/views/vehicle.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Vehicle
+from .serializers import VehicleSerializer
+
+class VehicleViewSet(ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
