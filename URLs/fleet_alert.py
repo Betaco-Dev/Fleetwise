756388f1e133b4c2b22a3e7492e83820114444a6
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 10:39:58 +0300

    Update fleet_alert.py

--- URLs/fleet_alert.py
+++ URLs/fleet_alert.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import FleetAlertViewSet
 
 router = DefaultRouter()
-router.register(r'fleet-alerts', FleetAlertViewSet, basename='fleetalert')
+router.register(r'fleet-alert', FleetAlertViewSet, basename='fleetalert')
 
 urlpatterns = [
     path('', include(router.urls)),
