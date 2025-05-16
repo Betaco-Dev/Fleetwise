3264a0195a52f417bb7697adc0f6b55c07510201
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 11:07:33 +0300

    Update and rename userpreference.py to user_preference.py

--- URLs/user_preference.py
+++ URLs/user_preference.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import UserPreferenceViewSet
 
 router = DefaultRouter()
-router.register(r'user-preferences', UserPreferenceViewSet, basename='userpreference')
+router.register(r'user-preference', UserPreferenceViewSet, basename='user_preference')
 
 urlpatterns = [
     path('', include(router.urls)),
