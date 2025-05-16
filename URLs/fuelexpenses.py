df83c7fd5ac40d50b7c687282e56e51b42e5a0b0
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-16 10:00:53 +0300

    Create fuelexpenses.py

---
+++ URLs/fuelexpenses.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import FuelExpenseViewSet
+
+router = DefaultRouter()
+router.register(r'fuel-expenses', FuelExpenseViewSet, basename='fuelexpense')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
