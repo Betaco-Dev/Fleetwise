3aec69b245e934024f74f408a730633b70e92c87
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-17 10:46:46 +0300

    Update analytics_reporting.py

--- Fleetwise/main/views/analytics_reporting.py
+++ Fleetwise/main/views/analytics_reporting.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import AnalyticsReport
 from .serializers import AnalyticsReportSerializer
+from .permissions import IsAdminOrReadOnlyExceptRestricted
 
 class AnalyticsReportViewSet(ModelViewSet):
     queryset = AnalyticsReport.objects.all()
     serializer_class = AnalyticsReportSerializer
+    permission_classes  = [IsAdminOrReadOnlyExceptRestricted]
