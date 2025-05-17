64c6933374785b2d728cc9ba2896715be47b4dc8
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-17 10:48:27 +0300

    Update and rename audit_log to audit_log.py

--- Fleetwise/main/views/audit_log.py
+++ Fleetwise/main/views/audit_log.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import AuditLog
 from .serializers import AuditLogSerializer
+from .permissions import IsAdminOrReadOnlyExceptRestricted
 
 class AuditLogViewSet(ModelViewSet):
     queryset = AuditLog.objects.all()
     serializer_class = AuditLogSerializer
+    permission_classes = [IsAdminOrReadOnlyExceptRestricted]
