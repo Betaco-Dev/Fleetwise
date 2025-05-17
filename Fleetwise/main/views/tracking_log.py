efbcb44a0d32402594246667cdf859843104efe3
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-17 10:43:56 +0300

    Update tracking_log.py

--- Fleetwise/main/views/tracking_log.py
+++ Fleetwise/main/views/tracking_log.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import TrackingLog
 from .serializers import trackingLogSerializer
+from .permissions import IsAdminOrReadOnlyExceptRestricted
 
 class TrackingLogViewSet(ModelViewSet):
     queryset = TrackingLog.objects.all()
     serializer_class = TrackingLogSerializer
+    permission_classes = [IsAdminOrReadOnlyExceptRestricted]
