0e1479cc4b682efe7c3866864cd625155892775c
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-17 17:23:48 +0300

    Update api_view.py

--- Fleetwise/main/api_view.py
+++ Fleetwise/main/api_view.py
@@ -2,8 +2,12 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from Fleetwise.main.serializers import TrackingLogSerializer
+from Fleetwise.main.serializers import (
+    TrackingLogSerializer,
+    RoutePlanSerializer
+)
 from Fleetwise.main.services.tracking_log_utils import reconstruct_trip_path
+from Fleetwise.main.services.route_plan_utils import check_overlapping_plans
 from Fleetwise.main.models.tracking_log import TrackingLog
 from Fleetwise.main.models.vehicle import Vehicle
 from .api_views import (
@@ -30,7 +34,7 @@ router.register('v1/delivery-updates', DeliveryUpdateViewSet)
 router.register('v1/analytics-reports', AnalyticsReportViewSet)
 router.register('v1/currencies', CurrencyViewSet)
 router.register('v1/audit-logs', AuditLogViewSet)
-router.register('v1/aipredictions', PredictionViewSet)
+router.register('v1/aipredictions', AiPredictionViewSet)
 router.register('v1/weather-conditions', WeatherConditionViewSet)
 router.register('v1/user-preferences', UserPreferenceViewSet)
 router.register('v1/fleet-alerts', FleetAlertViewSet)
@@ -40,8 +44,7 @@ router.register('v1/ai-maintenance', AiMaintenanceViewSet)
 app_name = 'Fleetwise'
 urlpatterns = router.urls
 
-#API Tracking log view
-
+# API Tracking log view
 class TrackingLogCreateAPIView(APIView):
     def post(self, request):
         serializer = TrackingLogSerializer(data=request.data)
@@ -54,4 +57,18 @@ class TripPathAPIView(APIView):
     def get(self, request, vehicle_id, trip_date):
         vehicle = Vehicle.objects.get(pk=vehicle_id)
         trip_points = reconstruct_trip_path(vehicle, trip_date)
-        return Response({'trip_points': trip_points}
+        return Response({'trip_points': trip_points})
+
+# API Route plan create view
+class RoutePlanCreateAPIView(APIView):
+    def post(self, request):
+        serializer = RoutePlanSerializer(data=request.data)
+        if serializer.is_valid():
+            vehicle = serializer.validated_data['vehicle']
+            start_date = serializer.validated_data['start_date']
+            end_date = serializer.validated_data['end_date']
+            if check_overlapping_plans(vehicle, start_date, end_date):
+                return Response({"detail": "Overlapping route plan exists."}, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
