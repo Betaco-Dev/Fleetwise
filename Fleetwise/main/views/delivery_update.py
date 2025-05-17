f8744b10f2c39176823e3d838b876669627884cf
Betaco-Dev <betacodevelopers@gmail.com>
2025-05-17 10:50:35 +0300

    Update delivery_update.py

--- Fleetwise/main/views/delivery_update.py
+++ Fleetwise/main/views/delivery_update.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import DeliveryUpdate
 from .serializers import DeliveryUpdateSerializer
+from .permissions import IsAdminOrReadOnlyExceptRestricted
 
 class DeliveryUpdatesViewSet(ModelViewSet):
     queryset = DeliveryUpdate.objects.all()
     serializer_class = DeliveryUpdateSerializer
+    permission_classes = [IsAdminOrReadOnlyExceptRestricted]
+    
